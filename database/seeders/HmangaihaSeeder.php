1f2e08863d6849679ec73dfdb187a3a1cf2e9a62
hmangaiha <hmangaiha.msegs@gmail.com>
2022-01-24 13:26:19 +0530

    Working test 2 till 5 legal

--- database/seeders/HmangaihaSeeder.php
+++ database/seeders/HmangaihaSeeder.php
@@ -4,11 +4,11 @@ namespace Database\Seeders;
 class HmangaihaSeeder {
     const DATA = [
         // ['code' => 'unique_code', 'title' => 'app title', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
-        ['code' => 'LEGAL_LICENCE_MANUFACTURING', 'title' => '', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
-        ['code' => 'LEGAL_LICENCE_RENEWAL_MANUFACTURER', 'title' => '   Application form for licence as manufacturer of weights & measures under the Legal Metrology Act, 2009', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
-        ['code' => 'LEGAL_DEALER_RENEWAL', 'title' => '', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
-        ['code' => 'LEGAL_LICENCE_REPAIR', 'title' => '', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
-        ['code' => 'LEGAL_LICENCE_DEALER', 'title' => '', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
+        ['code' => 'LEGAL_LICENCE_MANUFACTURING', 'title' => 'Application form for licence as manufacturer of weights & measures', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
+        ['code' => 'LEGAL_LICENCE_RENEWAL_MANUFACTURER', 'title' => 'Application for renewal Licence as Manufacturer of Weights & Measures', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
+        ['code' => 'LEGAL_DEALER_RENEWAL', 'title' => 'Application for renewal Licence as Manufacturer of Weights & Measures', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
+        ['code' => 'LEGAL_LICENCE_REPAIR', 'title' => 'Application for renewal Licence as Dealer in Weights & Measures ', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
+        ['code' => 'LEGAL_LICENCE_DEALER', 'title' => 'Application Form for Licence as Dealers in Weights & Measures', 'remark' => 'Remark', 'office_id' => 1, 'operational_type' => 'Post processing'],
 
     ];
 
