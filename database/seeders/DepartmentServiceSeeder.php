02440f9674f13c6ac673676fe66ebd5a33236f9a
hmangaiha <hmangaiha.msegs@gmail.com>
2022-02-23 11:06:03 +0530

    Link update:

--- database/seeders/DepartmentServiceSeeder.php
+++ database/seeders/DepartmentServiceSeeder.php
@@ -1075,7 +1075,7 @@ class DepartmentServiceSeeder extends Seeder
                 'document_to_submit' => $faker->word,
                 'timeline' => $faker->sentence,
                 'fees' => $faker->randomNumber(3),
-                'path' => "link",
+                'path' => "connected-load",
                 'sample_form' => $faker->sentence
             ],
             [
