061718161307ffc7839359bb7d9880acfac1d071
rjyoman485 <rjxtri485@gmail.com>
2022-03-01 16:51:15 +0530

    caf recovered

--- quasar-frontend/src/layouts/MainLayout.vue
+++ quasar-frontend/src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
     {{ $q.screen.gt.xs ? localData.openDrawer =  false : true }}
 
     <q-header  elevated  class="bg-white print-hide green-bottom-border text-primary" height-hint="98">
-      <q-toolbar v-if="!localData.onTop"  class="container-lg flex items-center">
+      <q-toolbar v-if="!localData.onTop"  class="container-lg flex items-center q-py-md">
         <q-btn class="lt-sm" @click="localData.openDrawer=true" flat icon="menu"/>
         <router-link to="/"
         ><img style="width: 80px" src="~assets/eodb-logo.png"
@@ -17,7 +17,7 @@
         <q-btn :to="{name:'process-flows:read'}" flat icon="settings"/>
       </q-toolbar>
       <q-separator/>
-      <q-toolbar class="scroll-y xs-hide container-lg q-pa-xs q-gutter-sm">
+      <q-toolbar class="scroll-y xs-hide container-lg q-pa-xs q-gutter-sm q-py-md">
 
         <q-btn  :to="{name:'staff:dashboard'}" :class="$route.name==='staff:dashboard' ?'selected-menu':'' "
                flat outline label="My desk" color="primary" icon="dashboard"/>
--- quasar-frontend/src/pages/public/staff/StaffLogin.vue
+++ quasar-frontend/src/pages/public/staff/StaffLogin.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page padding class="container-lg">
     <q-form class="q-mt-lg flex justify-center" @submit="submit" @reset="reset">
-      <div  style="max-width: 450px" class="zdetailcard row q-col-gutter-sm">
+      <div style="max-width: 450px" class="zdetailcard row q-col-gutter-sm">
         <div class="col-xs-12">
           <h1 class="ztitle">Login</h1>
         </div>
@@ -12,32 +12,36 @@
           <label class="zlabel" for="email">Email</label>
         </div>
         <div class="col-xs-12">
-          <q-input dense
-                   id="email"
-                   outlined
-                   v-model="formData.email"
-                   :error="[
-                   val=> !!val || 'Email is required',
-                   val=> emailRegex.test(val)  || 'Invalid email address'
-                 ]"
+          <q-input
+            dense
+            id="email"
+            outlined
+            v-model="formData.email"
+            :error="[
+              (val) => !!val || 'Email is required',
+              (val) => emailRegex.test(val) || 'Invalid email address',
+            ]"
           />
         </div>
         <div class="col-xs-12">
           <label class="zlabel" for="password">Password</label>
         </div>
         <div class="col-xs-12">
-          <q-input dense
-                   :type="localData.type"
-                   id="password"
-                   outlined
-                   v-model="formData.password"
-                   :error="[
-                   val=> !!val || 'Password is required',
-                 ]"
+          <q-input
+            dense
+            :type="localData.type"
+            id="password"
+            outlined
+            v-model="formData.password"
+            :error="[(val) => !!val || 'Password is required']"
           >
             <template v-slot:append>
               <q-icon
-                :name="localData.type==='password' ? 'visibility_off' : 'visibility'"
+                :name="
+                  localData.type === 'password'
+                    ? 'visibility_off'
+                    : 'visibility'
+                "
                 class="cursor-pointer"
                 @click="togglePassword"
               />
@@ -45,23 +49,22 @@
           </q-input>
         </div>
         <div class="col-xs-12">
-          <q-btn type="submit" label="login" no-caps no-wrap color="primary"/>
+          <q-btn type="submit" label="login" no-caps no-wrap color="primary" />
         </div>
       </div>
-
     </q-form>
-
   </q-page>
 </template>
 <script>
-import {reactive} from "@vue/reactivity";
-import {api} from "boot/axios";
-import {useQuasar} from "quasar";
-import {useStore} from "vuex";
-import {useRouter} from "vue-router";
-import {computed, onMounted} from "vue";
+import { reactive } from "@vue/reactivity";
+import { api } from "boot/axios";
+import { useQuasar } from "quasar";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import { computed, onMounted } from "vue";
 
-const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export default {
   setup(props, context) {
     const q = useQuasar();
@@ -69,53 +72,57 @@ export default {
     const router = useRouter();
 
     const localData = reactive({
-      type: 'password'
-    })
+      type: "password",
+    });
     const formData = reactive({
-      email: '',
-      password: ''
-    })
+      email: "",
+      password: "",
+    });
     const togglePassword = () => {
-      if (localData.type === 'password') {
-        localData.type = 'text'
+      if (localData.type === "password") {
+        localData.type = "text";
       } else {
-        localData.type = 'password'
+        localData.type = "password";
       }
-    }
+    };
     const submit = () => {
-      api.post('auth/staff-login', formData)
-        .then(res => {
-          const {token, user,permissions} = res.data;
-          console.log("user",user);
+      api
+        .post("auth/staff-login", formData)
+        .then((res) => {
+          const { token, user, permissions } = res.data;
+          // console.log("user", user);
           console.log(token);
-          store.dispatch('authData/setToken',token)
-          store.dispatch('authData/setCurrentUser',user)
-          store.dispatch('authData/setPermissions',permissions)
-          api.defaults.headers['Authorization'] = `Bearer ${token}`;
+          store.dispatch("authData/setToken", token);
+          console.log("user", user);
 
-          setTimeout(()=>{
-            router.push({name: 'staff:dashboard'});
-          },1000)
+          store.dispatch("authData/setCurrentUser", user);
+          store.dispatch("authData/setPermissions", permissions);
+          api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+          setTimeout(() => {
+            router.push({ name: "staff:dashboard" });
+          }, 1000);
         })
-        .catch(err => {
+        .catch((err) => {
           if (err?.response?.data?.errors)
-            localData.errors = err.response.data?.errors
-          err?.response?.data?.message && q.notify({
-            type: 'negative',
-            message: err.response?.data?.message
-          })
-        })
-    }
-    const isAuthenticated=computed(()=>store.getters['authData/isAuthenticated'])
+            localData.errors = err.response.data?.errors;
+          err?.response?.data?.message &&
+            q.notify({
+              type: "negative",
+              message: err.response?.data?.message,
+            });
+        });
+    };
+    const isAuthenticated = computed(
+      () => store.getters["authData/isAuthenticated"]
+    );
 
-    const reset = () => {
-
-    }
-    onMounted(()=>{
+    const reset = () => {};
+    onMounted(() => {
       // if (store.getters['authData/isAuthenticated']) {
       //   router.back();
       // }
-    })
+    });
     return {
       isAuthenticated,
       formData,
@@ -123,8 +130,8 @@ export default {
       emailRegex,
       togglePassword,
       submit,
-      reset
-    }
-  }
-}
+      reset,
+    };
+  },
+};
 </script>
